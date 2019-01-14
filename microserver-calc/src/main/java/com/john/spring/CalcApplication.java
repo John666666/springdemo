62d0ddfa9c44dcea8a750e2eca62b799e44c27e5
John <lijun_0372@163.com>
2019-01-14 17:07:58 +0800

    zipkin demo

--- microserver-calc/src/main/java/com/john/spring/CalcApplication.java
+++ microserver-calc/src/main/java/com/john/spring/CalcApplication.java
@@ -2,10 +2,10 @@ package com.john.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class CalcApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalcApplication.class, args);
